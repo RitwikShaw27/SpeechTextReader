aadf2685a1d5c8dd76c188eb058bfc03e35ef7de
Ritwik <shawritwik2@gmail.com>
2022-03-13 12:13:42 +0530

    Change voice and Custom Text Box

--- script.js
+++ script.js
@@ -70,7 +70,71 @@ function createBox(item) {
     <p class="info">${text}</p>
   `;
 
-  // @todo - speak event
+  box.addEventListener("click", () => {
+    setTextMessage(text);
+    speakText();
+
+    // Add active event
+    box.classList.add("active");
+    setTimeout(() => box.classList.remove("active"), 800);
+  });
 
   main.appendChild(box);
 }
+
+// Init speech synthesis
+const message = new SpeechSynthesisUtterance();
+
+// Store voices
+let voices = [];
+function getVoices() {
+  voices = speechSynthesis.getVoices();
+
+  voices.forEach((voice) => {
+    const option = document.createElement("option");
+
+    option.value = voice.name;
+    option.innerText = `${voice.name} ${voice.lang}`;
+
+    voicesSelect.appendChild(option);
+  });
+}
+
+// Set text
+function setTextMessage(text) {
+  message.text = text;
+}
+
+// Speak text
+function speakText() {
+  speechSynthesis.speak(message);
+}
+
+// Set voice
+function setVoice(e) {
+  message.voice = voices.find((voice) => voice.name === e.target.value);
+}
+
+// Voices changed
+speechSynthesis.addEventListener("voiceschanged", getVoices);
+
+// Toggle text box
+toggleBtn.addEventListener("click", () =>
+  document.getElementById("text-box").classList.toggle("show")
+);
+
+// Close button
+closeBtn.addEventListener("click", () =>
+  document.getElementById("text-box").classList.remove("show")
+);
+
+// Change voice
+voicesSelect.addEventListener("change", setVoice);
+
+// Read text button
+readBtn.addEventListener("click", () => {
+  setTextMessage(textarea.value);
+  speakText();
+});
+
+getVoices();
